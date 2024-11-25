e11117ef53b27604e0844b72017955dad9bec7be
maxim3638 <maximbortnik50@gmail.com>
2024-11-25 23:43:25 +0200

    Update практична робота №1.py

--- практична робота №1.py
+++ практична робота №1.py
@@ -1,13 +1,14 @@
-name = "Maxim"
-surname = "Bortnik"
-age = 16
-hello = 'Hello world!'
+hello = "Hello World" #створення зміної
+name = "Maxim" #створення зміної
+lastname = "<Bortnik" #створення зміної
+age = 16 #створення зміної
 
-print(hello)
-print(name)
-print(surname)
-print(age)
-print(type(hello))
-print(type(name))
-print(type(surname))
-print(type(age))
+print(hello) #Виведе значення змінної
+print(name) #Виведе значення змінної
+print(lastname) #Виведе значення змінної
+print(age) #Виведе значення змінної
+
+print(type(hello)) #Виведе значення змінної
+print(type(name)) #Виведе значення змінної
+print(type(lastname)) #Виведе значення змінної
+print(type(age)) #Виведе значення змінної
